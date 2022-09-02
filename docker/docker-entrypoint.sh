b48bab93d32d9039b28d193684d9e615d4eb715c
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2022-09-02 11:56:58 +0200

    More restrictive rights set to volumes

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 if [[ "$(whoami)" == root ]]; then
 	for F in /srv/{nwnx4-userdir,nwn2-home,nwn2-logs}; do
-		chown nwnx4:nwnx4 "$F"
-		chmod 755 "$F"
+		chown nwnx4 "$F"
+		chmod u+rwx "$F"
 	done
 fi
 
